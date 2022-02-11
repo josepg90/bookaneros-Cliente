8f338e47c2d4a1bcbea69481c3ad99b1fe1c7d0e
josepg90 <primogil.jose@gmail.com>
2022-02-11 13:02:57 +0100

    Implementado OpinionLista (no funciona aun)

--- src/app/component/application/unrouted/opinion/opinion-lista/opinion-lista.component.ts
+++ src/app/component/application/unrouted/opinion/opinion-lista/opinion-lista.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { IPagePost, IPost } from 'src/app/model/opinion-interfaces';
+import { IUsuario } from 'src/app/model/usuario-interfaces';
+import { IconService } from 'src/app/service/icon.service';
+import { OpinionService } from 'src/app/service/opinion.service';
+import { PaginationService } from 'src/app/service/pagination.service';
 
 @Component({
   selector: 'app-opinion-lista',
@@ -7,9 +14,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpinionListaComponent implements OnInit {
 
-  constructor() { }
+  strEntity: string = "post"
+  strOperation: string = "plist"
+  strTitleSingular: string = "Post";
+  strTitlePlural: string = "Posts";
+  aPosts: IPost[];
+  aPaginationBar: string[];
+  nTotalElements: number;
+  nTotalPages: number;
+  nPage: number;
+  nPageSize: number = 20;
+  strResult: string = null;
+  strFilter: string = "";
+  strSortField: string = "";
+  strSortDirection: string = "";
+  strFilteredMessage: string = "";
+  oUserSession: IUsuario;
+  subjectFiltro$ = new Subject();
+  barraPaginacion: string[];
+
+  genero: string;
+  id_libro: number;
+  id_usuario: number;
+  applyClass: string;
+
+  constructor(
+    private oRoute: ActivatedRoute,
+    private oRouter: Router,
+    private oPaginationService: PaginationService,
+    private oOpinionService: OpinionService,
+    public oIconService: IconService
+    ) { 
+      if (this.oRoute.snapshot.data.message) {
+      this.oUserSession = this.oRoute.snapshot.data.message;
+      localStorage.setItem("user", JSON.stringify(this.oRoute.snapshot.data.message));
+    } else {
+      localStorage.clear();
+      oRouter.navigate(['/home']);
+    }}
 
   ngOnInit(): void {
+  }
+
+  getPage = () => {
+    
+    console.log("buscando...", this.strFilter);
+
+    this.oOpinionService.getPage(this.nPageSize, this.nPage, this.strFilter, this.strSortField, this.strSortDirection, this.id_libro, this.id_usuario).subscribe((oPage: IPagePost) => {
+      if (this.strFilter) {
+        this.strFilteredMessage = "Listado filtrado: " + this.strFilter;
+      } else {
+        this.strFilteredMessage = "";
+      }
+      this.aPosts = oPage.content;
+      //this.randomLibros = _.shuffle(this.aLibros);
+      this.nTotalElements = oPage.totalElements;
+      this.nTotalPages = oPage.totalPages;
+      this.aPaginationBar = this.oPaginationService.pagination(this.nTotalPages, this.nPage);
+
+      console.log(this.id_libro);
+
+    })
+  }
+
+  jumpToPage = () => {
+    this.getPage();
+    return false;
+  }
+  onKeyUpFilter(event: KeyboardEvent): void {
+    this.subjectFiltro$.next();
+  }
+
+  doResetOrder() {
+    this.strSortField = "";
+    this.strSortDirection = "";
+    this.getPage();
+  }
+
+  doSetOrder(order: string) {
+    this.strSortField = order;
+    if (this.strSortDirection == 'asc') {
+      this.strSortDirection = 'desc';
+    } else if (this.strSortDirection == 'desc') {
+      this.strSortDirection = '';
+    } else {
+      this.strSortDirection = 'asc';
+    }
+    this.getPage();
   }
 
 }
